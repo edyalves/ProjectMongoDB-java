f30b7a9ab0a6f0d91129066e319fa84e9b9d1208
Edy Alves <edy4alves@gmail.com>
2021-06-23 17:06:29 -0300

    Conection MongoDB, repository and service implements

---
+++ src/main/java/com/edyco/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.edyco.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edyco.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
--- src/main/java/com/edyco/workshopmongo/resources/UserResource.java
+++ src/main/java/com/edyco/workshopmongo/resources/UserResource.java
@@ -4,23 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edyco.workshopmongo.domain.User;
+import com.edyco.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User pessoa1 = new User("1", "Pessoa 1", "Pessoa1@email.com");
-		User pessoa2 = new User("2", "Pessoa 2", "Pessoa2@email.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(pessoa1,pessoa2));
+		
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
